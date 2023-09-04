c30a217d36f4fce864d677102a7e6ce54be3437c
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-09-04 19:52:33 +0300

    employee find user by ID method

--- src/main/java/com/restapi/employeemanager/service/EmployeeService.java
+++ src/main/java/com/restapi/employeemanager/service/EmployeeService.java
@@ -28,6 +28,9 @@ public class EmployeeService {
         return employeeRepo.save(employee);
     }
 
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id);
+    }
     public void deleteEmployee(Long id){
         employeeRepo.deleteEmployeeById(id);
     }
