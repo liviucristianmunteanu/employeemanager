5e37a1ac77a2bb230f790f23fc2bf8a59ad0b44b
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-10-16 19:56:46 +0300

    created department entity

---
+++ src/main/java/com/restapi/employeemanager/model/Department.java
@@ -0,0 +1,25 @@
+package com.restapi.employeemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String imageUrl;
+
+}
